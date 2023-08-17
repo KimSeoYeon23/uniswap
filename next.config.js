f4ab454cf9580094295cca77cbdcd2e1a39b0b06
KimSeoYeon23 <sollucidus0221@naver.com>
2023-08-17 18:23:44 +0900

    Add initial setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+// JSDoc 주석, TypeScript를 사용하는 경우 이 주석을 통해 nextConfig 객체의 타입을 NextConfig로 지정하고 있다.
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = (phase, { defaultConfig }) => {
+  const nextConfig = {
+    // React의 Strict Mode 설정
+    // Strict Mode는 애플리케이션의 잠재적인 문제를 찾아내기 위해 React 컴포넌트를 더 엄격하게 검사하는 모드
+    reactStrictMode: true,
+  };
 
-module.exports = nextConfig
+  return nextConfig;
+};
---
+++ src/components/common/index.ts
@@ -0,0 +1,3 @@
+import Header from "../reference/Header";
+
+export { Header }
